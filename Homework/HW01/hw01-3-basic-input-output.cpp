6d0f0069398005373a16a9a8e7741ed6e39ee5fd
Mekha1104 <gtrcooie@gmail.com>
2023-08-16 21:26:35 +0700

    01-3

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -14,12 +14,37 @@
         ABD DEFs TC, RMUTL, Chiang Mai, Thailand
 */
 #include <stdio.h>
-int main(){
-    char FirstName[15] , LastName[15];
-    printf("First Name : ");
-    scanf("%s",&FirstName);
-    printf("Last Name : ");
-    scanf("%s",&LastName);
-    printf("%s %s TC, RMUTL, Chiang Mai, Thailand",FirstName,FirstName);
-    return 0 ;
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char firstName[100];
+    char lastName[100];
+
+    printf("First Name: ");
+    scanf("%s", firstName);
+
+    printf("Last Name: ");
+    scanf("%s", lastName);
+
+    // กำหนดรหัสสำหรับคณะ และสถานที่
+    char facultyCode[] = "TC";
+    char university[] = "RMUTL";
+    char location[] = "Chiang Mai, Thailand";
+
+    // แปลงตัวหนังสือแรกของนามสกุลให้เป็นตัวพิมพ์ใหญ่
+    lastName[0] = toupper(lastName[0]);
+
+    // แปลงตัวหนังสือทั้งหมดของชื่อและนามสกุลให้เป็นตัวพิมพ์เล็ก
+    for (int i = 0; i < strlen(firstName); i++) {
+        firstName[i] = tolower(firstName[i]);
+    }
+    for (int i = 0; i < strlen(lastName); i++) {
+        lastName[i] = tolower(lastName[i]);
+    }
+
+    // พิมพ์ชื่อและนามสกุลพร้อมกับข้อมูลอื่น ๆ
+    printf("%s %s %s, %s, %s", firstName, lastName, facultyCode, university, location);
+
+    return 0;
 }
